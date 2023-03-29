2c9022f8bebea64be413b8592de77b86cb36d630
Dawid Zmarzlik <dawid.zmarzlik@proton.me>
2023-03-29 08:09:00 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,83 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector<HTMLFormElement>('#new-task-form');
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const ClearButton = document.querySelector<HTMLButtonElement>('#clear-tasks');
+const tasks: Task[] = loadTaks();
+tasks.forEach(addListItem);
+
+ClearButton?.addEventListener('click', () => {
+  ClearTasks();
+});
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (input?.value == '' || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  tasks.push(newTask);
+  saveTasks();
+
+  addListItem(newTask);
+  input.value = '';
+});
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  const removeButton = document.createElement('button');
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  removeButton.innerText = 'X';
+  removeButton.addEventListener('click', () => {
+    removeTask(task.id);
+    item.remove();
+  });
+  item.append(removeButton);
+
+  list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem('TASKS', JSON.stringify(tasks));
+}
+
+function loadTaks(): Task[] {
+  const taskJSON = localStorage.getItem('TASKS');
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
+
+function ClearTasks() {
+  tasks.length = 0;
+  if (list != null) list.innerHTML = '';
+}
+
+function removeTask(id: string) {
+  const index = tasks.findIndex((task) => task.id === id);
+  if (index >= 0) {
+    tasks.splice(index, 1);
+    saveTasks();
+  }
+}
